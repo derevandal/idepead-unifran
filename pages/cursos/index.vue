82bfc20564a2ef1286ad0b3893eb4baa9bf0412e
André Van Dal <derevandal@users.noreply.github.com>
2017-10-09 17:13:46 -0300

    add pages: cursos e metodologia

---
+++ pages/cursos/index.vue
@@ -0,0 +1,26 @@
+<template lang='pug'>
+  article.container.article--content Cursos!
+</template>
+
+<script>
+// import sectionHeader from '~/components/sectionHeader.vue'
+// import sectionFooter from '~/components/sectionFooter.vue'
+// import articleContent from '~/components/articleContent.vue'
+
+// export default {
+//   components: {
+//     sectionHeader,
+//     sectionFooter,
+//     articleContent
+//   }
+// }
+</script>
+
+<style lang='stylus' >
+.article--content
+  background #fff
+  padding 3rem 5rem
+  border-radius 1px
+  flex-direction column
+  justify-content flex-start
+</style>
