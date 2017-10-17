840998a9344cbfec99242dac13086215c2f997b9
André Van Dal <derevandal@users.noreply.github.com>
2017-10-17 04:02:54 -0200

    TODO: image processor

--- nuxt.config.js
+++ nuxt.config.js
@@ -66,20 +66,37 @@ module.exports = {
           test: /\.(js|vue)$/,
           loader: 'eslint-loader',
           exclude: /(node_modules)/
-        })
-      }
-      if (ctx.dev) {
-        config.module.rules.push({
-          test: /\.(gif|png|jpe?g|svg)$/i,
-          use: [
-            'file-loader',
-            {
-              loader: 'image-webpack-loader',
-              options: {
-                bypassOnDebug: true
-              }
-            }
-          ]
+        // TODO
+        // },
+        // {
+        //   test: /\.(gif|png|jpe?g|svg)$/i,
+        //   use: [
+        //     'file-loader',
+        //     {
+        //       loader: 'image-webpack-loader',
+        //       options: {
+        //         mozjpeg: {
+        //           progressive: true,
+        //           quality: 65
+        //         },
+        //         // optipng.enabled: false will disable optipng
+        //         optipng: {
+        //           enabled: false
+        //         },
+        //         pngquant: {
+        //           quality: '65-90',
+        //           speed: 4
+        //         },
+        //         gifsicle: {
+        //           interlaced: false
+        //         },
+        //         // the webp option will enable WEBP
+        //         webp: {
+        //           quality: 75
+        //         }
+        //       }
+        //     }
+        //   ]
         })
       }
     }
