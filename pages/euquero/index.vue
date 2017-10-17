f855eeda9df02e313f352dd2b9f800e0aa0e00d2
André Van Dal <derevandal@users.noreply.github.com>
2017-10-17 18:58:47 -0200

    enable axios debug

--- pages/euquero/index.vue
+++ pages/euquero/index.vue
@@ -121,23 +121,23 @@
                   this.formSubmitted = true
                   this.$store.dispatch('CLEAR_INSCRIPTION')
                 })
-                .catch((e) => {
+                .catch((error) => {
                   if (error.response) {
-                      // The request was made and the server responded with a status code
-                      // that falls out of the range of 2xx
-                      // console.log(error.response.data);
-                      // console.log(error.response.status);
-                      // console.log(error.response.headers);
+                    // The request was made and the server responded with a status code
+                    // that falls out of the range of 2xx
+                    console.log(error.response.data)
+                    console.log(error.response.status)
+                    console.log(error.response.headers)
                   } else if (error.request) {
-                      // The request was made but no response was received
-                      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-                      // http.ClientRequest in node.js
-                      console.log(error.request)
+                    // The request was made but no response was received
+                    // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                    // http.ClientRequest in node.js
+                    console.log(error.request)
                   } else {
-                      // Something happened in setting up the request that triggered an Error
-                      console.log('Error', error.message)
+                    // Something happened in setting up the request that triggered an Error
+                    console.log('Error', error.message)
                   }
-                  console.log(error.config);
+                  console.log(error.config)
                 })
             })
             .catch((e) => {
