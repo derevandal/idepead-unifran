9ea47be62b0c53308957a39877b5c6e57edf6d24
André Van Dal <derevandal@users.noreply.github.com>
2017-10-17 18:55:43 -0200

    translate vee-validate

--- plugins/vee-validate.js
+++ plugins/vee-validate.js
@@ -1,4 +1,11 @@
 import Vue from 'vue'
-import VeeValidate from 'vee-validate'
 
-Vue.use(VeeValidate)
+import portuguese from 'vee-validate/dist/locale/pt_BR'
+
+import VeeValidate, { Validator } from 'vee-validate'
+
+Validator.addLocale(portuguese)
+
+Vue.use(VeeValidate, {
+  locale: 'pt_BR'
+})
