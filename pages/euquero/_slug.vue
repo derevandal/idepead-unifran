a6bf0c70a3247c1b7f3717249e04727c6fc7172c
André Van Dal <derevandal@users.noreply.github.com>
2017-10-17 02:57:57 -0200

    add form's inputs in 'euquero'

--- pages/euquero/_slug.vue
+++ pages/euquero/_slug.vue
@@ -1,6 +1,30 @@
 <template lang='pug'>
   div
-    p slug
+    .form-group
+      input(type="text" v-model="name" required)
+      label.control-label(for="input") Como podemos te chamar?
+      i.bar
+    .form-group
+      input(type="email" v-model="email" required)
+      label.control-label(for="input") Qual seu e-mail?
+      i.bar
+    .form-group
+      input(type="phone" v-model="phone" required)
+      label.control-label(for="input") Qual seu telefone?
+      i.bar
+    .form-group
+      select(required v-model="selected")
+        option(value="-1") Selecione seu curso...
+        option(v-for="course in allCourses" :key="course.slug" :value="course.slug" :selected="course.slug === slug") {{ course.name }}
+      label.control-label(for="select") Qual o curso desejado?
+      i.bar
+    .checkbox
+      label
+        input(type="checkbox" checked)
+        i.helper
+        | Desejo que entrem em contato comigo
+    .button-container
+      button.button(type="submit"): span Enviar
 
 </template>
 
@@ -9,7 +33,11 @@
     name: 'euquero-with-slug',
     data () {
       return {
-        slug: null
+        name: null,
+        email: null,
+        phone: null,
+        selected: null,
+        slug: undefined
       }
     },
     created () {
@@ -21,6 +49,7 @@
     methods: {
       initSlug () {
         this.slug = this.$route.params.slug.trim()
+        this.selected = this.slug
         if (this.slug) {
           this.$store.dispatch('FILTERCOURSES_BY_SLUG', this.slug)
           this.$store.dispatch('SET_DOMAIN', '/euquero/' + this.slug)
@@ -31,6 +60,9 @@
     computed: {
       getCourseName () {
         return this.$store.getters.getFilteredCourse.name || ''
+      },
+      allCourses () {
+        return (this.$store.getters.allCourses)
       }
     }
   }
--- pages/euquero/index.vue
+++ pages/euquero/index.vue
@@ -1,13 +1,54 @@
 <template lang='pug'>
   div
-    p no slug
+    .form-group
+      input(type="text" v-model="name" required)
+      label.control-label(for="input") Como podemos te chamar?
+      i.bar
+    .form-group
+      input(type="email" v-model="email" required)
+      label.control-label(for="input") Qual seu e-mail?
+      i.bar
+    .form-group
+      input(type="phone" v-model="phone" required)
+      label.control-label(for="input") Qual seu telefone?
+      i.bar
+    .form-group
+      select(required)
+        option(v-if="this.slug" value="-1" name="select") Selecione seu curso...
+        option(v-else value="-1" name="select" select) Selecione seu curso...
+        option(v-for="course in allCourses" :key="course.slug" :selected="isSelected" ) {{ course.name }}
+      label.control-label(for="select") Qual o curso desejado?
+      i.bar
+    .checkbox
+      label
+        input(type="checkbox" checked)
+        i.helper
+        | Desejo que entrem em contato comigo
+    .button-container
+      button.button(type="submit"): span Enviar
 </template>
 
 <script>
   export default {
     name: 'eu-quero-no-slug',
+    data () {
+      return {
+        name: null,
+        email: null,
+        phone: null,
+        slug: undefined
+      }
+    },
     created () {
       this.setPage()
+    },
+    computed: {
+      allCourses () {
+        return (this.$store.getters.allCourses)
+      },
+      isSelected () {
+        return false
+      }
     },
     watch: {
       '$route': 'setPage'
