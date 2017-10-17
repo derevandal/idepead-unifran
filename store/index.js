5ec79b6fa3621bad8a20ee01fc6d9d5c5726a751
André Van Dal <derevandal@users.noreply.github.com>
2017-10-17 18:56:09 -0200

    add inscripption in store

--- store/index.js
+++ store/index.js
@@ -37,7 +37,16 @@ const store = () => new Vuex.Store({
     courses,
     searchWord: null,
     filteredCourses: null,
-    filteredCourse: null
+    filteredCourse: null,
+    inscription: {
+      name: null,
+      email: null,
+      phone: null,
+      course: 1,
+      agreed: true,
+      token: null
+    },
+    axiosErrors: {}
   },
   getters: {
 
@@ -45,7 +54,7 @@ const store = () => new Vuex.Store({
 
     getTitle: (state) => state.seo.title,
 
-    getFullDescription: (state) => state.seo.getFullDescription,
+    getFullDescription: (state) => state.seo.description,
 
     allCourses: (state) => state.courses,
 
@@ -53,12 +62,28 @@ const store = () => new Vuex.Store({
 
     getFilteredCourses: (state) => state.filteredCourses,
 
-    getFilteredCourse: (state) => state.filteredCourse
+    getFilteredCourse: (state) => state.filteredCourse,
+
+    getInscription: (state) => state.inscription,
+
+    getInscriptionName: (state) => state.inscription.name,
+    getInscriptionEmail: (state) => state.inscription.email,
+    getInscriptionPhone: (state) => state.inscription.phone,
+    getInscriptionCourse: (state) => state.inscription.course,
+    getInscriptionAgreed: (state) => state.inscription.agreed,
+    getInscriptionToken: (state) => state.inscription.token,
+
+    getAxiosErros: (state) => state.axiosErrors
   },
   mutations: {
     SET_COURSE (state, course) {
       state.course = course
     },
+
+    SET_AXIOS_ERRORS (state, errors) {
+      state.axiosErrors = errors
+    },
+
     FILTER_COURSES (state, word) {
       if (!(word) || word === '{}') {
         state.searchWord = null
@@ -84,11 +109,40 @@ const store = () => new Vuex.Store({
     },
     SET_TITLE (state, title) {
       state.seo.title = title
+    },
+    SET_INSCRIPTION_NAME (state, name) {
+      state.inscription.name = name
+    },
+    SET_INSCRIPTION_EMAIL (state, email) {
+      state.inscription.email = email
+    },
+    SET_INSCRIPTION_PHONE (state, phone) {
+      state.inscription.phone = phone
+    },
+    SET_INSCRIPTION_COURSE (state, course) {
+      state.inscription.course = course
+    },
+    SET_INSCRIPTION_AGREED (state, agreed) {
+      state.inscription.agreed = agreed
+    },
+    SET_INSCRIPTION_TOKEN (state, token) {
+      state.inscription.token = token
+    },
+    CLEAR_INSCRIPTION (state) {
+      state.inscription.name = null
+      state.inscription.email = null
+      state.inscription.phon = null
+      state.inscription.course = 1
+      state.inscription.agreed = true
+      state.inscription.token = null
     }
   },
   actions: {
     SET_COURSE ({ commit }, course) {
       commit('SET_COURSE', course)
+    },
+    SET_AXIOS_ERRORS ({ commit }, errors) {
+      commit('SET_AXIOS_ERRORS', errors)
     },
     FILTER_COURSES ({ commit }, word) {
       commit('FILTER_COURSES', word)
@@ -101,6 +155,30 @@ const store = () => new Vuex.Store({
     },
     SET_TITLE ({ commit }, title) {
       commit('SET_TITLE', title)
+    },
+    SET_INSCRIPTION_NAME ({ commit }, title) {
+      commit('SET_INSCRIPTION_NAME', title)
+    },
+    SET_INSCRIPTION_EMAIL ({ commit }, title) {
+      commit('SET_INSCRIPTION_EMAIL', title)
+    },
+    SET_INSCRIPTION_PHONE ({ commit }, title) {
+      commit('SET_INSCRIPTION_PHONE', title)
+    },
+    SET_INSCRIPTION_COURSE ({ commit }, title) {
+      commit('SET_INSCRIPTION_COURSE', title)
+    },
+    SET_INSCRIPTION_AGREED ({ commit }, title) {
+      commit('SET_INSCRIPTION_AGREED', title)
+    },
+    SET_INSCRIPTION_TOKEN ({ commit }, title) {
+      commit('SET_INSCRIPTION_TOKEN', title)
+    },
+    SET_INSCRIPTION_SUBMITTED ({ commit }, title) {
+      commit('SET_INSCRIPTION_SUBMITTED', title)
+    },
+    CLEAR_INSCRIPTION ({ commit }) {
+      commit('CLEAR_INSCRIPTION')
     }
   }
 })
